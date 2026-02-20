178405645485f6fd7d9320cb133251c1c227fccb
Sam <sam@samdalton.co.uk>
2026-02-20 22:38:25 +0000

    SAM-001: Basic text edit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,16 @@ export default function Home() {
             To get started, edit the page.tsx file.
           </h1>
           <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
+Tier 0: Foundations (mandatory)
+
+Hangul (alphabet)
+
+Recognize letters (read)
+
+Write letters (type/handwrite)
+
+Sound-to-letter and letter-to-sound (listening/speaking-ish)
+Unlocks: everything else.            <a
               href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
               className="font-medium text-zinc-950 dark:text-zinc-50"
             >
