c426920585718adb4ee69260a29633611482eab5
Katie D <katie@katedalton.co.uk>
2026-02-20 21:57:05 +0000

    Colour Change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-pink-600">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-purple-800 sm:items-start">
         <Image
-          className="dark:invert"
+          className="animate-spin dark:invert"
           src="/next.svg"
           alt="Next.js logo"
           width={100}
